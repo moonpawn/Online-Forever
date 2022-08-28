4cbf3bb5e5e583eaa631d1257fe129753a16c777
moonpawn <108006450+moonpawn@users.noreply.github.com>
2022-08-28 13:43:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 from keep_alive import keep_alive
 from discord.ext import commands
 
+name_of_stream_status = "Hello World" #Name that show on stream status, Ex : {username} streaming [...]
+stream_link = "https://twitch.tv/CustomStreamLink" #Must be a twitch link only, nothing else will work 
 client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 
 @client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online)
+  await client.change_presence(activity = discord.Streaming(name = f"{name_of_stream_status}", url = f"{stream_link}"))
   os.system('clear')
   print(f'Logged in as {client.user} (ID: {client.user.id})')
 
 keep_alive()
-client.run(os.getenv("TOKEN"))
+client.run(os.getenv("TOKEN")) #Must be a secret on replit, if you are hosting this script without replit or if you have a hacker plan on, check for the code needed on the readme.md
